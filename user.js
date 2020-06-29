036d9db507338746028069d0a5349166e8e3ca3e
FlysJoint <wattoo42@hotmail.com>
2020-06-29 13:24:48 +0100

    initial backend setup

---
+++ user.js
@@ -0,0 +1,129 @@
+"use strict";
+const express = require('express');
+const serverless = require('serverless-http');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: "quake_beacon"
+});
+
+app.get("/user", function(req, res) {
+  const query = "SELECT * FROM user;"
+
+  connection.query(query, function(err, data) {
+    if (err) {
+      console.log("Error fetching user", err);
+      res.status(500).json({
+        error: err
+      });
+    } else {
+      res.json({
+        user: data
+      });
+    }
+  });
+});
+
+app.post("/user", function(req, res) {
+  const query = "INSERT INTO user (user_email, user_phone, user_language, user_firstName, user_lastName, user_lat, user_long, user_inDangerZone, user_markedSafe) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);";
+  const querySelect = "SELECT * FROM user WHERE userID = ?";
+
+  connection.query(query, [req.body.user_email, req.body.user_phone, req.body.user_language, req.body.user_firstName, req.body.user_lastName, req.body.user_lat, req.body.user_long, req.body.user_inDangerZone, req.body.user_markedSafe], function(error, data) {
+    if(error) {
+      console.log("Error adding a user", error);
+      res.status(500).json({
+        error: error
+      })
+    } else {
+      connection.query(querySelect, [data.insertId], function(error, data) {
+        if(error) {
+          console.log("Error getting the user", error);
+          res.status(500).json({
+            error: error
+          })
+        } else {
+          res.status(201).json({
+            user: data
+          })
+        }
+      })
+    }
+  })
+});
+
+app.put('/user/:userID', function(req, res) {
+  const query = "UPDATE user SET user_email = ?, user_phone = ?, user_language = ?, user_firstName = ?, user_lastName = ?, user_lat = ?, user_long = ?, user_inDangerZone =?, user_markedSafe = ? WHERE userID = ?;";
+  const querySelect = "SELECT * FROM user WHERE userID = ?";
+
+  connection.query(query, [req.body.user_email, req.body.user_phone, req.body.user_language, req.body.user_firstName, req.body.user_lastName, req.body.user_lat, req.body.user_long, req.body.user_inDangerZone, req.body.user_markedSafe, req.params.userID], function(error, data) {
+    if(error) {
+      console.log("Error updating a user", error);
+      res.status(500).json({
+        error: error
+      })
+    } else {
+      connection.query(querySelect, [req.params.userID], function(error, data) {
+        if(error) {
+          console.log("Error getting the user", error);
+          res.status(500).json({
+            error: error
+          })
+        } else {
+          res.status(200).json({
+            users: data
+          })
+        }
+      })
+    }
+  })
+});
+
+app.delete('/user/:userID', function(req, res) {
+  const query = "DELETE FROM user WHERE userID = ?;";
+  const querySelect = "SELECT * FROM user"; // to verify its deleted, printing them all out seems like overkill though
+
+  connection.query(query, [req.params.userID], function(error, data) {
+    if(error) {
+      console.log("Error deleting a user", error);
+      res.status(500).json({
+        error: error
+      })
+    } else {
+      connection.query(querySelect, function(error, data) {
+        if(error) {
+          console.log("Error getting the user", error);
+          res.status(500).json({
+            error: error
+          })
+        } else {
+          res.status(200).json({
+            users: data
+          })
+        }
+      })
+    }
+  })
+});
+
+
+module.exports.handler = serverless(app);
+
+// module.exports.handler = async (event, context) => {
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify({
+//       message: "10-4 good buddy",
+//       input: event
+//     })
+//   };
+// };
